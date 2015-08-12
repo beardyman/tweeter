e93c2112873945a289cdf4e67dc3102a019ae794
nornholdj <nornholdj@gmail.com>
2015-08-11 21:59:06 -0400

    Added endpoint for posting retweets

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,21 @@ module.exports = function (streamable) {
   /*
    * Make Twitter things!!!
    */
-  router.get('/statuses/filter.json', streamable, function (req, res, next) {
+  router.get('/statuses/filter.json', streamable, function (req, res) {
+    console.log('connection made');
     setInterval(function () {
       res.write(tweet.getTweet(), 'json');
     }, 1000);
 
     res.write(tweet.getTweet(), 'json');
+  });
+
+  router.post('/statuses/retweet/:id', function (req, res) {
+    console.log('posting to ' + req.params.id);
+    console.log(req.body);
+    console.log('\n\n');
+
+    res.sendStatus(200);
   });
 
   return router;
